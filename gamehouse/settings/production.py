f7d5ac50596c9e17b7043365dc9255eb30f8c620
Martin Rogel <martin.mimr@gmail.com>
2020-12-27 16:54:30 -0600

    First attempt to solve database problem

--- gamehouse/settings/production.py
+++ gamehouse/settings/production.py
@@ -12,7 +12,17 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES = {}
+DATABASES = { 
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+		'NAME':, 
+		'USER':, 
+		'PASSWORD':, 
+		'HOST':, 
+		'PORT':,
+	}
+}
+
 DATABASES['default'].update(db_from_env)
 
 
