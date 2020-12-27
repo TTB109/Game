60e35d550e7e8cd244ade7461c684e0b662af358
Martin Rogel <martin.mimr@gmail.com>
2020-12-27 16:40:33 -0600

    Trying to solve errors during deployment

--- gamehouse/settings/production.py
+++ gamehouse/settings/production.py
@@ -12,6 +12,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES = {}
 DATABASES['default'].update(db_from_env)
 
 
