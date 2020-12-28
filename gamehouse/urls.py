294039144c1dc0a01193585a3c30c7a963ab23eb
Martin Rogel <martin.mimr@gmail.com>
2020-12-28 08:08:48 -0600

    Settings for local applied

--- gamehouse/urls.py
+++ gamehouse/urls.py
@@ -1,18 +1,3 @@
-"""gamehouse URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.views.generic import TemplateView as tv
 from django.urls import path, include, re_path,reverse
 
@@ -48,11 +33,9 @@ urlpatterns = [
     path('sadm/',include('gamehouse.sadm.urls')), 
 ]
 
-ADMIN_ENABLED = config('ADMIN_ENABLED', default=False, cast=bool)
-if ADMIN_ENABLED is True:
+if settings.ADMIN_ENABLED is True:
 	from django.contrib import admin
 	urlpatterns += [path('admin/', admin.site.urls),]
-
 
 """ Para modo local
 
